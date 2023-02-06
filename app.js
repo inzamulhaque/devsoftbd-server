f775951de7599ee3c9aa2dba8e8274c87b508e81
inzamulhaque <mdihalif@gmail.com>
2023-02-07 01:35:50 +0600

    add service api

--- app.js
+++ app.js
@@ -12,13 +12,10 @@ const blogRoute = require("./routes/blog.route");
 const reviewRoute = require("./routes/review.route");
 const contactRoute = require("./routes/contact.route");
 const tellUsRoute = require("./routes/tellUs.route");
-<<<<<<< HEAD
 const subscribeRoute = require("./routes/subscribe.route");
 const teamRoute = require("./routes/team.route");
 const pricingRoute = require("./routes/pricing.route");
-=======
 const serviceRoute = require("./routes/service.route");
->>>>>>> cf458a06e0845d8690246f8a9762ecd734594bbc
 
 // routes
 app.use("/api/v1/user", userRoute);
@@ -26,13 +23,10 @@ app.use("/api/v1/blog", blogRoute);
 app.use("/api/v1/review", reviewRoute);
 app.use("/api/v1/contact", contactRoute);
 app.use("/api/v1/tellus", tellUsRoute);
-<<<<<<< HEAD
 app.use("/api/v1/subscribe", subscribeRoute);
 app.use("/api/v1/team", teamRoute);
 app.use("/api/v1/pricing", pricingRoute);
-=======
 app.use("/api/v1/service", serviceRoute);
->>>>>>> cf458a06e0845d8690246f8a9762ecd734594bbc
 
 app.get("/", (req, res) => {
   res.send("Route is working!");
